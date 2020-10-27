0b36a42d3ec0fc97d02baff7dc1717c266cab81b
Rebar Hassan <rebar.hassan@immowelt.de>
2020-10-27 18:38:29 +0100

    Initial commit.

--- lib/c3-1-stack.ts
+++ lib/c3-1-stack.ts
@@ -1,18 +1,14 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class C31Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'C31Queue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'C31Topic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const lambdaFunction = new lambda.Function(this, `${this.stackName}-lambdaFunction`,{
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'function.handler'
+    })
   }
 }
